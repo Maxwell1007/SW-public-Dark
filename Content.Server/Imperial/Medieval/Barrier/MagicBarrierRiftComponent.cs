5440a20d22a1cfa6d522c86900c2b31d0deaf1c6
Maxwell1007 <84186058+Maxwell1007@users.noreply.github.com>
2026-01-19 12:25:55 +0300

    Adjust elemental rifts and add guardian locks

---
+++ Content.Server/Imperial/Medieval/Barrier/MagicBarrierRiftComponent.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace Content.Server.MagicBarrier.Components
+{
+    [RegisterComponent]
+    public sealed partial class MagicBarrierRiftComponent : Component
+    {
+        [DataField]
+        public string Element = "earth";
+
+        [DataField]
+        public bool GuardiansSpawned;
+
+        [DataField]
+        public List<EntityUid> Guardians = new();
+    }
+}
